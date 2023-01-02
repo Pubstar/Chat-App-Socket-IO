b1f57f5e76386d065a33113e52920d3a2c1dd81a
Pubstar <pieterhoek@msn.com>
2023-01-02 22:04:18 +0100

    Added Project

---
+++ public/client.js
@@ -0,0 +1,55 @@
+var socket = io();
+const send_button = document.getElementById('send-button');
+const messagesList = document.getElementById('messagesList');
+const input = document.getElementById('inputText');
+const messagesDiv = document.querySelector('.messages');
+const username = document.getElementById('username');
+const userCountText = document.getElementById('userCountText');
+
+const scrollMessages = () => {
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
+
+input.addEventListener('keypress', (e) => {
+    if (e.key == "Enter") send_button.click();
+})
+
+send_button.addEventListener('click', () => {
+    if (username.value.length < 1 || input.value.length < 1) return;
+    socket.emit('message', { sender: username.value, message: input.value });
+    input.value = '';
+    input.focus();
+})
+
+socket.on('updateUsers', (data) => {
+    userCountText.innerText = "Users in chat: " + data;
+})
+
+socket.on('message', (data) => {
+    let cardEl = document.createElement('div');
+    cardEl.classList.add('card');
+    cardEl.classList.add('mt-3');
+    cardEl.classList.add('w-50');
+    let cardBodyEl = document.createElement('div');
+    cardBodyEl.classList.add('card-body');
+    let blockQuote = document.createElement('blockquote');
+    blockQuote.classList.add('blockquote');
+    blockQuote.classList.add('mb-0');
+    let messageEl = document.createElement('p');
+    messageEl.innerText = data.message;
+    let footerEl = document.createElement('footer');
+    footerEl.classList.add('blockquote-footer');
+    footerEl.innerText = data.sender;
+
+    if (data.sender == username.value) {
+        cardEl.classList.add('setRight');
+        cardEl.classList.add('me-3');
+        cardEl.classList.add('text-bg-dark')
+    }
+    blockQuote.append(messageEl, footerEl);
+    cardBodyEl.append(blockQuote);
+    cardEl.append(cardBodyEl);
+
+    messagesList.append(cardEl);
+    scrollMessages();
+})
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+let users = 0;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    users++;
+    socket.on('disconnect', () => {
+        users--;
+        io.emit('updateUsers', users);
+    })
+    socket.on('message', (data) => {
+        io.emit('message', data);
+    })
+
+    io.emit('updateUsers', users);
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
